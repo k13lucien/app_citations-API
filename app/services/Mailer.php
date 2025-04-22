72b18568bb0f92792fc46f3f1facad3c45935624
k13lucien <lucienkiemde4@gmail.com>
2025-04-22 15:13:42 +0000

    version 1.0

--- app/services/Mailer.php
+++ app/services/Mailer.php
@@ -16,13 +16,13 @@ class Mailer
             $mail->isSMTP();
             $mail->Host       = 'smtp.gmail.com';
             $mail->SMTPAuth   = true;
-            $mail->Username   = 'filskiemde13@gmail.com'; 
-            $mail->Password   = 'mzst ivih caxd sahp';
+            $mail->Username   = 'tonadresse@gmail.com'; 
+            $mail->Password   = 'mot de passe';
             $mail->SMTPSecure = 'tls';  
             $mail->Port       = 587;  
 
             // Infos de l'e-mail
-            $mail->setFrom('filskiemde134@gmail.com', 'Citations App');
+            $mail->setFrom('tonadresse@gmail.com', 'Citations App');
             $mail->addAddress($to);
             $mail->isHTML(true);
             $mail->Subject = $subject;
